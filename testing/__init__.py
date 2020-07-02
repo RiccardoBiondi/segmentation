da78c85643c0ab5f86e848f06dd60eda44a4ccf9
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2020-07-02 15:44:20 +0200

    Added test, coverage reports and pycache

---
+++ testing/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import segmentation.method
+__author__  = ['Riccardo Biondi','Nico Curti']
+__email__ = ['riccardo.biondi4@studio.unibo.it','nico.curti2@unibo.it']
