d8c6af708f9b84f2bc61a8bd29b843c506ef1a78
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2020-08-24 12:32:43 +0200

    Major updates

---
+++ labeling.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+red='\033[1;31m'
+green='\033[1;32m'
+reset='\033[0m]' #No Color
+
+
+input_dir=$1
+output_dir=$2
+centroids=$3
+
+
+
+# check if the input directory exists and it is provided
+if [ -z "$input_dir" ]; then
+
+  echo -e "${red}Error! Input directory not set${reset}"
+  exit 1
+
+elif [ ! -d "$input_dir" ]; then
+
+  echo -e "${red}Error! Input directory not found${reset}"
+  exit 1
+
+fi
+
+# check if the output directory exists and it is provided
+if [ -z "$output_dir" ]; then
+
+  echo -e "${red}Error! Output directory not set${reset}"
+  exit 1
+
+# comment the following lines if
+# the output directory could not exist
+elif [ ! -d "$output_dir" ]; then
+
+  echo -e "${red}Error! Output directory not found${reset}"
+  exit 1
+
+fi
+
+#chck if the centroids file is provided
+if [ -z "$centroids" ]; then
+
+  echo -e "${red}Error! Centroid file not set${reset}"
+  exit 1
+
+#check if the centroid file exist
+elif [ ! -f "$centroids" ]; then
+
+  echo -e "${red}Error ! Centroid file not found${reset}"
+  exit 1
+
+fi
+
+# list all the pickle files into the input directory
+input_files=$(ls $input_dir)
+echo "Found ${#input_files[@]} files to process"
+
+# define a logfile
+logfile=$PWD/log_processing.txt
+
+# eventually create the output directory
+mkdir -p $output_dir
+
+# apply the pipeline on the input files
+
+for file in $input_files; do
+
+  printf "* Processing $file ...       "
+
+  #f= basename "$file" .pkl.npy
+
+  f="${file%%.*}"
+  
+  python3 -m pipeline.labeling --input=$input_dir$file --output=$output_dir$f --centroids=$centroids
+
+  if [ $? = 0 ]; then
+    echo -e "${green}[done]${reset}"
+  else
+    echo -e "${red}[failed]${reset}"
+    exit 1 # you can omit this line if you want to catch
+           # possible errors into the log without an exit
+  fi
+
+done
---
+++ train.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+red='\033[1;31m'
+green='\033[1;32m'
+reset='\033[0m]' #No Color
+
+input_dir=$1
+output_file=$2
+optional1=$3
+optional2=$4
+optional3=$5
+optional4=$6
+
+
+
+# check if the input directory exists and it is provided
+if [ -z "$input_dir" ]; then
+
+  echo -e "${red}Error! Input directory not set${reset}"
+  exit 1
+
+elif [ ! -d "$input_dir" ]; then
+
+  echo -e "${red}Error! Input directory not found${reset}"
+  exit 1
+
+fi
+
+
+# check if the output directory exists and it is provided
+if [ -z "$output_file" ]; then
+
+  echo -e "${red}Error! Output file not set${reset}"
+  exit 1
+
+fi
+
+# apply the pipeline on the input files
+
+python3 -m pipeline.train --input=$input_dir --output=$output_file $optional1 $optional2 $optional3 $optional4
