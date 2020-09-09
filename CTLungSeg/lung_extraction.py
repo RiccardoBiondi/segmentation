fdb8e36512b2b42f2616936716ba45166b6ea45b
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2020-09-09 23:18:36 +0200

    minor changes

--- CTLungSeg/lung_extraction.py
+++ CTLungSeg/lung_extraction.py
@@ -35,41 +35,31 @@ def main():
 
     args = parse_args()
     DICOM = load_image(args.input)
-    DICOM = preprocess(DICOM)
 
-    lung_mask = otsu_threshold(gaussian_blur(DICOM, (5,5)))
+    lung_mask = otsu_threshold(gaussian_blur(preprocess(DICOM), (5,5)))
     lung_mask = opening(lung_mask, kernel=np.ones((5,5), dtype=np.uint8))
     lung_mask = opening(lung_mask, kernel=np.ones((11,11), dtype=np.uint8))
 
     body_mask = imfill(lung_mask)
-    body_mask = np.where(body_mask == 255, 1, 0)
+    body_mask[body_mask == 255] = 1
 
-    lung_mask = np.where(lung_mask == 0, 1, 0)
-    lung_mask = lung_mask * body_mask
+    lung_mask = body_mask * np.logical_not(lung_mask)
     #filter out internal and external spots
-    #se inverti forse va meglio!! meno righe di codice
-    lung_mask = np.where(lung_mask == 0, 1, 0)
-    lung_mask = remove_spots(lung_mask, args.isa)
-    lung_mask = np.where(lung_mask == 0, 1, 0)
-    lung_mask = remove_spots(lung_mask, args.esa)
+    lung_mask = remove_spots(np.logical_not(lung_mask), args.isa)
+    lung_mask = remove_spots(np.logical_not(lung_mask), args.esa)
 
     lung_mask = reconstruct_gg_areas(lung_mask)
-
-    save_pickle(args.lung + '_mask',lung_mask )
     #BIT PLANE slices
     bit_lung_mask = gl2bit(lung_mask * DICOM, 8)
     t_mask = get_bit(bit_lung_mask, 5) + get_bit(bit_lung_mask, 7) + get_bit(bit_lung_mask, 8)
-    t_mask = gaussian_blur(t_mask, (7,7))
-    t_mask = preprocess(t_mask)
+    t_mask = otsu_threshold(preprocess(gaussian_blur(t_mask, (7,7))))
 
-    t_mask = otsu_threshold(t_mask)
-    t_mask = np.where(t_mask == 0, 1, 0)
-
-    lung_mask = (t_mask * lung_mask).astype(np.uint8)
+    lung_mask = t_mask * lung_mask
     lung_mask = closing(lung_mask, np.ones((7,7), dtype=np.uint8))
 
     lung_mask = select_greater_connected_regions(lung_mask, 2)
     lung_mask[lung_mask == 255] = 1
+
     DICOM = lung_mask * DICOM
 
 
