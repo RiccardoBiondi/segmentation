d5bd3e97439ae5b84e121c74981402a127eb2d46
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2020-09-09 17:27:27 +0200

    Update the labeling algorithm

--- CTLungSeg/labeling.py
+++ CTLungSeg/labeling.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import cv2
 import argparse
 from sklearn.cluster import KMeans
 from CTLungSeg.utils import load_image, save_pickle
+from CTLungSeg.method import median_blur
 
 __author__ = ['Riccado Biondi', 'Nico Curti']
 __email__  = ['riccardo.biondi4@tudio.unibo.it', 'nico.curti2@unibo.it']
@@ -30,8 +32,35 @@ def main():
     shape = images.shape
     images = images.reshape((-1,1))
     res = KMeans(n_clusters=n_clusters, init=centroid, n_init=1).fit(images)
+    labels = res.labels_.reshepe(shape)
+    labels = median_blur(labels, 5)
+    #create mask from labels
+    labels[labels == 2] = 0
+    lebels[labels == 3] = 1
+    lebels = opening(labels, np.ones((5,5), np.uint8))
+    #
+    images = labels * images
+    #variabili del secondo clustering
+    images = preprocess(images)
+    #applica un filtro mediano
+    images = medianBlur(images, 9)
+    images = images.astype(np.float32)
+    #
+    criteria = (cv2.TERM_CRITERIA_EPS +cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    init = [cv2.KMEANS_RANDOM_CENTERS, cv2.KMEANS_PP_CENTERS]
 
-    save_pickle(args.out, res.labels_.reshape(shape))
+    _, labels,_ = cv2.kmeans(images.reshape(-1,), 3, None,criteria, 10, init[1])
+
+    labels = labels.reshape(images.shape)
+    #
+    labels = labels.astype(np.uint8)
+    labels = median_blur(labels, 7)
+    labels[labels == 2] = 1
+
+
+
+
+    save_pickle(args.out, labels)
 
 
 if __name__ == '__main__' :
