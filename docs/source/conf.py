680aa02ab9002586c0116af22a8f34129421d4fb
RiccardoBiondi <riccardo.biondi4@studio.unibo.it>
2021-09-01 12:01:07 +0200

    [DOCS] Updated doc version. Fix github actions badges hyperlinks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, RiccardoBiondi, Nico Curti'
 author = 'RiccardoBiondi, Nico Curti'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '2.0.0'
 master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
